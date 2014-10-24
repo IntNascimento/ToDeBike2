9f8f137b93774ef400a60a0028352d85dae55f4f
Fabio Rehm <fgrehm@gmail.com>
2014-10-24 18:02:57 -0200

    Script ruby para conversão de CSV com acidentes para JSON com latitude e longitude

---
+++ scripts/convert-accidents-to-json.rb
@@ -0,0 +1,21 @@
+# encoding: utf-8
+
+if ARGV.size != 1
+  puts 'Utilização: `ruby scripts/convert-accidents-to-json.rb <ARQUIVO-CSV> > <ARQUIVO_JSON>`'
+  exit 1
+end
+
+require 'csv'
+require 'json'
+
+data = []
+CSV.foreach(ARGV[0], col_sep: ';', headers: true) do |row|
+  next if row['BICICLETA'].to_i == 0
+
+  data << {
+    latitude:  row['LATITUDE'].gsub(',', '.').to_f,
+    longitude: row['LONGITUDE'].gsub(',', '.').to_f
+  }
+end
+
+puts "window.acidentes = #{data.to_json};"
