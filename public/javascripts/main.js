0bb256e5be9b9aadb31b7a3e01fd6b6cbd9399d3
Fabio Rehm <fgrehm@gmail.com>
2014-10-29 00:08:02 -0200

    Exibe estações do BikePOA no mapa

--- public/javascripts/main.js
+++ public/javascripts/main.js
@@ -15,19 +15,39 @@ ToDeBike.initGoogleMap = function() {
 google.maps.event.addDomListener(window, 'load', ToDeBike.initGoogleMap);
 
 // Add a single marker to the map using the provided coordinates
-ToDeBike.addMarker = function(lat, lng, title) {
+ToDeBike.addMarker = function(lat, lng, title, icon) {
   new google.maps.Marker({
     position: new google.maps.LatLng(lat, lng),
+    icon: icon,
     map: ToDeBike.map,
     title: title
   });
 };
 
 // Asynchronous loading of accidents involving bikes
-ToDeBike.loadAccidents = function(jsonAccidentsPath) {
-  $.getJSON(jsonAccidentsPath, function(data) {
+ToDeBike.loadAccidents = function(accidentsJsonPath) {
+  $.getJSON(accidentsJsonPath, function(data) {
     for (var i = 0; i < data.length; i++) {
-      ToDeBike.addMarker(data[i].latitude, data[i].longitude, "Acidente");
+      ToDeBike.addMarker(
+        data[i].latitude,
+        data[i].longitude,
+        "Acidente",
+        "http://maps.google.com/mapfiles/ms/icons/red-dot.png"
+      );
+    }
+  });
+};
+
+// Asynchronous loading of BikePOA stations
+ToDeBike.loadBikePOAStations = function(jsonBikePOAPath) {
+  $.getJSON(jsonBikePOAPath, function(data) {
+    for (var i = 0; i < data.length; i++) {
+      ToDeBike.addMarker(
+        data[i].latitude,
+        data[i].longitude,
+        "Estação '" + data[i].name + "'",
+        "http://maps.google.com/mapfiles/ms/icons/yellow-dot.png"
+      );
     }
   });
 };
