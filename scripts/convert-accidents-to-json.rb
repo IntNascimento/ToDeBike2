0afd58a4200946617f92cee8285556ec1c7430da
Fabio Rehm <fgrehm@gmail.com>
2014-11-06 20:32:33 -0200

    Atualiza script para gerar informacoes adicionais sobre acidentes

--- scripts/convert-accidents-to-json.rb
+++ scripts/convert-accidents-to-json.rb
@@ -12,10 +12,23 @@ data = []
 CSV.foreach(ARGV[0], col_sep: ';', headers: true) do |row|
   next if row['BICICLETA'].to_i == 0
 
+  vehicles = %w( AUTO TAXI LOTACAO ONIBUS_URB ONIBUS_MET ONIBUS_INT CAMINHAO MOTO CARROCA OUTRO ).map do |type|
+    type if row[type].to_i > 0
+  end
+
   data << {
-    latitude:  row['LATITUDE'].gsub(',', '.').to_f,
-    longitude: row['LONGITUDE'].gsub(',', '.').to_f
+    latitude:     row['LATITUDE'].gsub(',', '.').to_f,
+    longitude:    row['LONGITUDE'].gsub(',', '.').to_f,
+    fatal:        (row['FATAIS'].to_i > 0),
+    injuried:     row['FERIDOS'].to_i,
+    type:         row['TIPO_ACID'],
+    address:      row['LOCAL_VIA'],
+    region:       row['REGIAO'],
+    date:         row['DATA_HORA'].split.first,
+    time:         row['DATA_HORA'].split.last,
+    day_or_night: row['NOITE_DIA'],
+    vehicles:     vehicles.compact
   }
 end
 
-puts "#{data.to_json}"
+puts data.to_json
