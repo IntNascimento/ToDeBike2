034a18d442ad42e5366954180a606a6583b6693f
IntNascimento <int.nascimento@gmail.com>
2014-11-25 01:33:37 -0200

    Update main.js

--- js/main.js
+++ js/main.js
@@ -52,7 +52,7 @@ ToDeBike.loadBikePOAStations = function(jsonBikePOAPath) {
         data[i].latitude,
         data[i].longitude,
         "Estação '" + data[i].name + "'",
-        "../images/icones/bike.png"
+        "../images/icons/bike.png"
       );
     }
     ToDeBike.centerMap();
