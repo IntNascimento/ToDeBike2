b90c53f171813c11210ca7fa6d732a3ca7556c89
IntNascimento <int.nascimento@gmail.com>
2014-11-25 18:06:50 -0200

    Update main.js

--- js/main.js
+++ js/main.js
@@ -93,13 +93,26 @@ ToDeBike.loadAccidents = function(accidentsJsonPath) {
 };
 
 
-  var loadScriptTime = (new Date).getTime();
-  getScript("https://maps.gstatic.com/maps-api-v3/api/js/18/15a/intl/pt_br/main.js");
-})();
-// Asynchronous loading of BikePOA stations
-ToDeBike.loadBikePOAStations = function(apiLoad) {
-    apiLoad([0.009999999776482582,[[["https://mts0.googleapis.com/vt?lyrs=m@280000000\u0026src=api\u0026hl=pt-BR\u0026","https://mts1.googleapis.com/vt?lyrs=m@280000000\u0026src=api\u0026hl=pt-BR\u0026"],null,null,null,null,"m@280000000",["https://mts0.google.com/vt?lyrs=m@280000000\u0026src=api\u0026hl=pt-BR\u0026","https://mts1.google.com/vt?lyrs=m@280000000\u0026src=api\u0026hl=pt-BR\u0026"]],[["https://khms0.googleapis.com/kh?v=162\u0026hl=pt-BR\u0026","https://khms1.googleapis.com/kh?v=162\u0026hl=pt-BR\u0026"],null,null,null,1,"162",["https://khms0.google.com/kh?v=162\u0026hl=pt-BR\u0026","https://khms1.google.com/kh?v=162\u0026hl=pt-BR\u0026"]],[["https://mts0.googleapis.com/vt?lyrs=h@280000000\u0026src=api\u0026hl=pt-BR\u0026","https://mts1.googleapis.com/vt?lyrs=h@280000000\u0026src=api\u0026hl=pt-BR\u0026"],null,null,null,null,"h@280000000",["https://mts0.google.com/vt?lyrs=h@280000000\u0026src=api\u0026hl=pt-BR\u0026","https://mts1.google.com/vt?lyrs=h@280000000\u0026src=api\u0026hl=pt-BR\u0026"]],[["https://mts0.googleapis.com/vt?lyrs=t@132,r@280000000\u0026src=api\u0026hl=pt-BR\u0026","https://mts1.googleapis.com/vt?lyrs=t@132,r@280000000\u0026src=api\u0026hl=pt-BR\u0026"],null,null,null,null,"t@132,r@280000000",["https://mts0.google.com/vt?lyrs=t@132,r@280000000\u0026src=api\u0026hl=pt-BR\u0026","https://mts1.google.com/vt?lyrs=t@132,r@280000000\u0026src=api\u0026hl=pt-BR\u0026"]],null,null,[["https://cbks0.googleapis.com/cbk?","https://cbks1.googleapis.com/cbk?"]],[["https://khms0.googleapis.com/kh?v=84\u0026hl=pt-BR\u0026","https://khms1.googleapis.com/kh?v=84\u0026hl=pt-BR\u0026"],null,null,null,null,"84",["https://khms0.google.com/kh?v=84\u0026hl=pt-BR\u0026","https://khms1.google.com/kh?v=84\u0026hl=pt-BR\u0026"]],[["https://mts0.googleapis.com/mapslt?hl=pt-BR\u0026","https://mts1.googleapis.com/mapslt?hl=pt-BR\u0026"]],[["https://mts0.googleapis.com/mapslt/ft?hl=pt-BR\u0026","https://mts1.googleapis.com/mapslt/ft?hl=pt-BR\u0026"]],[["https://mts0.googleapis.com/vt?hl=pt-BR\u0026","https://mts1.googleapis.com/vt?hl=pt-BR\u0026"]],[["https://mts0.googleapis.com/mapslt/loom?hl=pt-BR\u0026","https://mts1.googleapis.com/mapslt/loom?hl=pt-BR\u0026"]],[["https://mts0.googleapis.com/mapslt?hl=pt-BR\u0026","https://mts1.googleapis.com/mapslt?hl=pt-BR\u0026"]],[["https://mts0.googleapis.com/mapslt/ft?hl=pt-BR\u0026","https://mts1.googleapis.com/mapslt/ft?hl=pt-BR\u0026"]],[["https://mts0.googleapis.com/mapslt/loom?hl=pt-BR\u0026","https://mts1.googleapis.com/mapslt/loom?hl=pt-BR\u0026"]]],["pt-BR","US",null,0,null,null,"https://maps.gstatic.com/mapfiles/","https://csi.gstatic.com","https://maps.googleapis.com","https://maps.googleapis.com",null,"https://maps.google.com"],["https://maps.gstatic.com/maps-api-v3/api/js/18/15a/intl/pt_br","3.18.15a"],[2204465565],1,null,null,null,null,null,"",null,null,1,"https://khms.googleapis.com/mz?v=162\u0026",null,"https://earthbuilder.googleapis.com","https://earthbuilder.googleapis.com",null,"https://mts.googleapis.com/vt/icon",[["https://mts0.googleapis.com/vt","https://mts1.googleapis.com/vt"],["https://mts0.googleapis.com/vt","https://mts1.googleapis.com/vt"],null,null,null,null,null,null,null,null,null,null,["https://mts0.google.com/vt","https://mts1.google.com/vt"],"/maps/vt",280000000,132],2,500,["https://geo0.ggpht.com/cbk","https://g0.gstatic.com/landmark/tour","https://g0.gstatic.com/landmark/config","","https://www.google.com/maps/preview/log204","","https://static.panoramio.com.storage.googleapis.com/photos/",["https://geo0.ggpht.com/cbk","https://geo1.ggpht.com/cbk","https://geo2.ggpht.com/cbk","https://geo3.ggpht.com/cbk"]],["https://www.google.com/maps/api/js/master?pb=!1m2!1u18!2s15a!2spt-BR!3sUS!4s18/15a/intl/pt_br","https://www.google.com/maps/api/js/widget?pb=!1m2!1u18!2s15a!2spt-BR"],1,0], loadScriptTime);
-  };
+ // Asynchronous loading of BikePOA stations
+ToDeBike.loadBikePOAStations = function(jsonBikePOAPath) {
+  $.getJSON(jsonBikePOAPath, function(data) {
+    for (var i = 0; i < data.length; i++) {
+      var info = '<p>';
+      info += '<img src="images/bikepoa/' + data[i].name.replace(/\s+/gm, '_') + '.jpg">';
+      info += '</p>';
+
+      ToDeBike.addMarker(
+        data[i].latitude,
+        data[i].longitude,
+        "Estação '" + data[i].name + "'",
+        "images/icons/bike.png",
+        info
+      );
+    }
+    ToDeBike.centerMap();
+  });
+};
+
 // Centers map based on known markers
 ToDeBike.centerMap = function() {
   var latlngbounds = new google.maps.LatLngBounds();
